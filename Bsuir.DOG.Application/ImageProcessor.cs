2b0c5c7f4e0d682831f797b3b9ff025fc5dfda34
Dmitry Vasilevich <wotlk777@gmail.com>
2019-11-03 19:04:56 +0300

    added image processing methods

---
+++ Bsuir.DOG.Application/ImageProcessor.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bsuir.DOG.Application
+{
+    public static class ImageProcessor
+    {
+        public static Bitmap ConvolutionFilter(Bitmap source, double[,] filter, double factor = 1, int bias = 0, bool isGrayscale = false)
+        {
+            BitmapData sourceData = source.LockBits(
+                new Rectangle(0, 0, source.Width, source.Height), 
+                ImageLockMode.ReadOnly, 
+                PixelFormat.Format32bppArgb
+            );
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            source.UnlockBits(sourceData);
+
+            if (isGrayscale == true)
+            {
+                for (int k = 0; k < pixelBuffer.Length; k += 4)
+                {
+                    float rgb = pixelBuffer[k] * 0.11f;
+                    rgb += pixelBuffer[k + 1] * 0.59f;
+                    rgb += pixelBuffer[k + 2] * 0.3f;
+
+                    pixelBuffer[k] = (byte)rgb;
+                    pixelBuffer[k + 1] = pixelBuffer[k];
+                    pixelBuffer[k + 2] = pixelBuffer[k];
+                    pixelBuffer[k + 3] = 255;
+                }
+            }
+
+            int filterWidth = filter.GetLength(1);
+            //int filterHeight = filter.GetLength(0);
+
+            int filterOffset = (filterWidth - 1) / 2;
+            for (int offsetY = filterOffset; offsetY < source.Height - filterOffset; offsetY++)
+            {
+                for (int offsetX = filterOffset; offsetX < source.Width - filterOffset; offsetX++)
+                {
+                    double blue = 0;
+                    double green = 0;
+                    double red = 0;
+
+                    int byteOffset = offsetY * sourceData.Stride + offsetX * 4;
+                    for (int filterY = -filterOffset; filterY <= filterOffset; filterY++)
+                    {
+                        for (int filterX = -filterOffset; filterX <= filterOffset; filterX++)
+                        {
+                            int calcOffset = byteOffset + (filterX * 4) + (filterY * sourceData.Stride);
+
+                            blue += pixelBuffer[calcOffset] * filter[filterY + filterOffset, filterX + filterOffset];
+                            green += pixelBuffer[calcOffset + 1] * filter[filterY + filterOffset, filterX + filterOffset];
+                            red += pixelBuffer[calcOffset + 2] * filter[filterY + filterOffset, filterX + filterOffset];
+                        }
+                    }
+
+                    blue = factor * blue + bias;
+                    green = factor * green + bias;
+                    red = factor * red + bias;
+
+                    blue = blue < 0 ? 0 : (blue > 255 ? 255 : blue);
+                    green = green < 0 ? 0 : (green > 255 ? 255 : green);
+                    red = red < 0 ? 0 : (red > 255 ? 255 : red);
+
+                    resultBuffer[byteOffset] = (byte)blue;
+                    resultBuffer[byteOffset + 1] = (byte)green;
+                    resultBuffer[byteOffset + 2] = (byte)red;
+                    resultBuffer[byteOffset + 3] = 255;
+                }
+            }
+
+            Bitmap result = new Bitmap(source.Width, source.Height);
+
+            BitmapData resultData = result.LockBits(
+                new Rectangle(0, 0, result.Width, result.Height),
+                ImageLockMode.WriteOnly,
+                PixelFormat.Format32bppArgb
+            );
+
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+            result.UnlockBits(resultData);
+
+            return result;
+        }
+
+        public static Bitmap SubtractImages(Bitmap source, Bitmap value, bool invert = false, int bias = 0)
+        {
+            BitmapData sourceData = source.LockBits(
+                new Rectangle(0, 0, source.Width, source.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format32bppArgb);
+
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, resultBuffer, 0, resultBuffer.Length);
+
+            BitmapData subtractData = value.LockBits(
+                new Rectangle(0, 0, value.Width, value.Height),
+                ImageLockMode.ReadOnly, 
+                PixelFormat.Format32bppArgb);
+
+            byte[] subtractBuffer = new byte[subtractData.Stride * subtractData.Height];
+
+            Marshal.Copy(subtractData.Scan0, subtractBuffer, 0, subtractBuffer.Length);
+
+            value.UnlockBits(subtractData);
+
+            for (int k = 0; k < resultBuffer.Length && k < subtractBuffer.Length; k += 4)
+            {
+                int red;
+                int green;
+                int blue;
+
+                if (invert == true)
+                {
+                    blue = 255 - resultBuffer[k] - subtractBuffer[k] + bias;
+                    green = 255 - resultBuffer[k + 1] - subtractBuffer[k + 1] + bias;
+                    red = 255 - resultBuffer[k + 2] - subtractBuffer[k + 2] + bias;
+                }
+                else
+                {
+                    blue = resultBuffer[k] - subtractBuffer[k] + bias;
+                    green = resultBuffer[k + 1] - subtractBuffer[k + 1] + bias;
+                    red = resultBuffer[k + 2] - subtractBuffer[k + 2] + bias;
+                }
+
+                blue = blue < 0 ? 0 : (blue > 255 ? 255 : blue);
+                green = green < 0 ? 0 : (green > 255 ? 255 : green);
+                red = red < 0 ? 0 : (red > 255 ? 255 : red);
+
+                resultBuffer[k] = (byte)blue;
+                resultBuffer[k + 1] = (byte)green;
+                resultBuffer[k + 2] = (byte)red;
+                resultBuffer[k + 3] = 255;
+            }
+
+            Marshal.Copy(resultBuffer, 0, sourceData.Scan0,
+                                       resultBuffer.Length);
+
+            source.UnlockBits(sourceData);
+            return source;
+        }
+
+        public static Bitmap DifferenceOfGaussians(Bitmap source, MatrixType filterFirst, MatrixType filterSecond, bool grayscale = false, bool invert = false, int bias = 0)
+        {
+            Bitmap convolvedFirst = ConvolutionFilter(source, 
+                Matrix.GaussianInstance(filterFirst), 
+                Matrix.GetGaussianFactor(filterFirst), 
+                0, grayscale);
+            Bitmap convolvedSecond = ConvolutionFilter(source, 
+                Matrix.GaussianInstance(filterSecond), 
+                Matrix.GetGaussianFactor(filterSecond), 
+                0, grayscale);
+
+            var result = SubtractImages(convolvedFirst, convolvedSecond, invert, bias);
+
+            return result;
+        }
+    }
+}
---
+++ Bsuir.DOG.Application/Matrix.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bsuir.DOG.Application
+{
+    public enum MatrixType
+    {
+        Gaussian3x3,
+        Gaussian5x5Type1,
+        Gaussian5x5Type2
+    }
+
+    public static class Matrix
+    {
+        public static double[,] GaussianInstance(MatrixType type)
+        {
+            switch (type)
+            {
+                case MatrixType.Gaussian3x3:
+                    return new double[,]
+                    {
+                        { 1, 2, 1 },
+                        { 2, 4, 2 },
+                        { 1, 2, 1 },
+                    };
+                case MatrixType.Gaussian5x5Type1:
+                    return new double[,]
+                    {
+                        { 2, 04, 05, 04, 2 },
+                        { 4, 09, 12, 09, 4 },
+                        { 5, 12, 15, 12, 5 },
+                        { 4, 09, 12, 09, 4 },
+                        { 2, 04, 05, 04, 2 },
+                    };
+                case MatrixType.Gaussian5x5Type2:
+                    return new double[,]
+                    {
+                        { 1,   4,  6,  4,  1 },
+                        { 4,  16, 24, 16,  4 },
+                        { 6,  24, 36, 24,  6 },
+                        { 4,  16, 24, 16,  4 },
+                        { 1,   4,  6,  4,  1 },
+                    };
+                default:
+                    return null;
+            }
+        }
+
+        public static double GetGaussianFactor(MatrixType type)
+        {
+            switch (type)
+            {
+                case MatrixType.Gaussian3x3:
+                    return 1.0 / 16.0;
+                case MatrixType.Gaussian5x5Type1:
+                    return 1.0 / 159.0;
+                case MatrixType.Gaussian5x5Type2:
+                    return 1.0 / 256.0;
+                default:
+                    return -1;
+            }
+        }
+    }
+}
